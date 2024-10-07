2a960f5d3d92a9be133f35fcaf63a93781703c25
Scott Render <64731759+scottsss123@users.noreply.github.com>
2024-10-07 18:59:00 +0100

    2

--- NeuralNet.h
+++ NeuralNet.h
@@ -2,17 +2,23 @@
 
 class NeuralNet {
 public:
-
-private:
-	std::vector<std::vector<float>> values;
-	std::vector<std::vector<float>> weights;
-	std::vector < std::vector<float>> biases;
-
-public:
-	void init_weights();
-private:
-	float dotProduct(std::vector<float> A, std::vector<float> B);
-	float reLU(float num);
-	float sigmoid(float num);
-	std::vector<float> softmax(std::vector<float> vec);
+	int n_inputs;
+	int n_layers;
+	std::vector<std::vector<std::vector<float>>> weights;
 };
+
+
+// WEIGHTS   MATRIX  (n(inputs) * n(nodes in layer 1))     ACTIVATION OF INPUTS VECTOR 
+//
+//  w0,0, w0,1, ...                                        A0
+//  w1,0, w1,1, ...										   A1
+//  ...
+//  ...
+//  ...
+//  ...
+
+// MATRIX MULT ^
+//    LAYER 1 WEIGHTS                     L2  L3
+//  [ [[w0,0,  w0,1...],[w1,0, w1,1...]], [], [] ]
+//  Size of weights = number of layers in network - 1
+//  Size of weights[i] = size of layer i ( can be multiplied by size of layer i + 1 )
