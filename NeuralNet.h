b2312c6d7cba60adcb88adc02f22cc4f3f18f441
Scott Render <64731759+scottsss123@users.noreply.github.com>
2024-10-08 21:56:26 +0100

    3
    Structure of NeuralNet.h practically copied from Sebastian Lague

--- NeuralNet.h
+++ NeuralNet.h
@@ -1,24 +1,79 @@
 #include "includes.h"
 
-class NeuralNet {
+class Layer {
+private:
+	int numNodesIn, numNodesOut;
+	std::vector<std::vector<double>> weights;
+	std::vector<double> biases;
+
 public:
-	int n_inputs;
-	int n_layers;
-	std::vector<std::vector<std::vector<float>>> weights;
+	Layer(int nNodesIn, int nNodesOut) {
+		numNodesIn = nNodesIn;
+		numNodesOut = nNodesOut;
+
+		weights.reserve(numNodesIn);
+		for (auto row : weights) {
+			row.reserve(numNodesOut);
+		}
+		biases.reserve(numNodesOut);
+	}
+
+	void InitRandomWeights() {
+
+	}
+
+	double Sigmoid(double x) {
+		double numerator = 1;
+		double denominator = 1 + std::exp(-x);
+		return numerator / denominator;
+	}
+
+	std::vector<double> CalculateOutputs(std::vector<double> inputs) {
+		std::vector<double> weightedInputs(numNodesOut);
+
+		for (int nodeOut = 0; nodeOut < numNodesOut; nodeOut++) {
+			double weightedInput = biases[nodeOut];
+			for (int nodeIn = 0; nodeIn < numNodesIn; nodeIn++) {
+				weightedInput += inputs[nodeIn] * weights[nodeIn][nodeOut];
+			}
+			weightedInputs[nodeOut] = Sigmoid(weightedInput);
+		}
+
+		return weightedInputs;
+	}
 };
 
+class NeuralNet {
+private:
+	std::vector<Layer> layers;
+public:
+	NeuralNet(std::vector<int> layerSizes) {
+		layers.reserve(layerSizes.size() - 1);
+		for (int i = 0; i < layers.size(); i++) {
+			layers[i] = Layer(layerSizes[i], layerSizes[i + 1]);
+		}
+	}
 
-// WEIGHTS   MATRIX  (n(inputs) * n(nodes in layer 1))     ACTIVATION OF INPUTS VECTOR 
-//
-//  w0,0, w0,1, ...                                        A0
-//  w1,0, w1,1, ...										                     A1
-//  ...
-//  ...
-//  ...
-//  ...
+	std::vector<double> CalculateOutputs(std::vector<double> inputs) {
+		std::vector<double> nextInputs = inputs;
+		for (auto layer : layers) {
+			nextInputs = layer.CalculateOutputs(nextInputs);
+		}
+		return nextInputs;
+	}
 
-// MATRIX MULT ^
-//    LAYER 1 WEIGHTS                     L2  L3
-//  [ [[w0,0,  w0,1...],[w1,0, w1,1...]], [], [] ]
-//  Size of weights = number of layers in network - 1
-//  Size of weights[i] = size of layer i ( can be multiplied by size of layer i + 1 )
+	std::vector<double> Classify(std::vector<double> inputs) {
+		std::vector<double> outputs = CalculateOutputs(inputs);
+		
+		double maxFound = - 1;
+		int index = 0;
+		for (int i = 0; i < outputs.size(); i++) {
+			if (outputs[i] > maxFound) {
+				maxFound = outputs[i];
+				index = i;
+			}
+		}
+
+		std::vector<double> classification = { (double)index, maxFound };
+	}
+};
