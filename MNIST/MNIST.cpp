fe15ecf9f47afb6bb21f0f104af3006e118fd6ce
Scott Render <64731759+scottsss123@users.noreply.github.com>
2025-01-29 15:51:13 +0000

    deleted:    .vs/MNIST/FileContentIndex/4bd23ff1-1680-4b9c-8c66-9109d6bea41e.vsidx 	deleted:    .vs/MNIST/FileContentIndex/b9bf288c-d596-4bf6-bcc6-5391ca1e8585.vsidx 	deleted:    .vs/MNIST/FileContentIndex/dc877c6c-79c4-415e-b0da-969cdae17347.vsidx 	deleted:    .vs/MNIST/FileContentIndex/read.lock 	deleted:    .vs/MNIST/config/applicationhost.config 	deleted:    .vs/MNIST/v17/.suo 	deleted:    .vs/MNIST/v17/Browse.VC.db 	deleted:    .vs/MNIST/v17/ipch/AutoPCH/97950f9c3bade371/MAIN.ipch 	deleted:    .vs/MNIST/v17/ipch/AutoPCH/fa9e5357f17495c1/MAIN.ipch 	deleted:    .vs/ProjectSettings.json 	deleted:    .vs/VSWorkspaceState.json 	deleted:    .vs/slnx.sqlite 	deleted:    MNIST/.vs/MNIST/FileContentIndex/056948e6-f79b-4867-86ba-e5e9ed4ccb98.vsidx 	deleted:    MNIST/.vs/MNIST/FileContentIndex/07a43520-0513-40c2-83ce-b366663b3148.vsidx 	deleted:    MNIST/.vs/MNIST/FileContentIndex/read.lock 	deleted:    MNIST/.vs/MNIST/v17/.suo 	deleted:    MNIST/.vs/MNIST/v17/Browse.VC.db 	deleted:    MNIST/.vs/MNIST/v17/Browse.VC.db-shm 	deleted:    MNIST/.vs/MNIST/v17/Browse.VC.db-wal 	deleted:    MNIST/.vs/MNIST/v17/Browse.VC.opendb 	deleted:    MNIST/.vs/MNIST/v17/ipch/AutoPCH/434d836708e56e33/MNIST.ipch 	modified:   MNIST/MNIST.cpp 	deleted:    MNIST/MNIST.sln 	deleted:    MNIST/MNIST.vcxproj 	deleted:    MNIST/MNIST.vcxproj.filters 	deleted:    MNIST/MNIST.vcxproj.user 	deleted:    MNIST/x64/Debug/MNIST.exe 	deleted:    MNIST/x64/Debug/MNIST.exe.recipe 	deleted:    MNIST/x64/Debug/MNIST.ilk 	deleted:    MNIST/x64/Debug/MNIST.log 	deleted:    MNIST/x64/Debug/MNIST.obj 	deleted:    MNIST/x64/Debug/MNIST.pdb 	deleted:    MNIST/x64/Debug/MNIST.tlog/CL.command.1.tlog 	deleted:    MNIST/x64/Debug/MNIST.tlog/CL.read.1.tlog 	deleted:    MNIST/x64/Debug/MNIST.tlog/CL.write.1.tlog 	deleted:    MNIST/x64/Debug/MNIST.tlog/MNIST.lastbuildstate 	deleted:    MNIST/x64/Debug/MNIST.tlog/link.command.1.tlog 	deleted:    MNIST/x64/Debug/MNIST.tlog/link.read.1.tlog 	deleted:    MNIST/x64/Debug/MNIST.tlog/link.write.1.tlog 	deleted:    MNIST/x64/Debug/MNIST.vcxproj.FileListAbsolute.txt 	deleted:    MNIST/x64/Debug/vc143.idb 	deleted:    MNIST/x64/Debug/vc143.pdb

--- MNIST/MNIST.cpp
+++ MNIST/MNIST.cpp
@@ -1,67 +1,97 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <fstream>
 
-// random float gen, dist(e2);
+const std::string training_images_filepath = "./train-images.idx3-ubyte";
+const std::string training_labels_filepath = "./train-labels.idx1-ubyte";
+
+
+// random float gen, e.g. rand_real(e2);
 std::random_device rd;
 std::mt19937 e2(rd());
-std::uniform_real_distribution<> dist(-1, 1); 
-
-class Node {
-public:
-    float activation;
-    float bias;
-    std::vector<float> weights;
+std::uniform_real_distribution<> rand_real(0, 1); 
 
 
-    Node(int n_inputs) {
-        weights.resize(n_inputs);
+//28x28
+std::vector<std::vector<unsigned char>> readImages(const std::string& filePath) {
+	std::ifstream file(filePath, std::ios::binary);
 
-        bias = dist(e2);
-        for (int i = 0; i < weights.size(); i++) {
-            weights[i] = dist(e2);
-        }
+	std::cout << "loading " << filePath << "\n";
 
-        activation = 0;
-    }
-};
+	char magic_number_bytes[4];
+	char n_images_bytes[4];
+	char n_rows_bytes[4];
+	char n_cols_bytes[4];
 
-class Layer {
-public:
-    std::vector<Node> node_arr;
+	file.read(magic_number_bytes, 4);
+	file.read(n_images_bytes, 4);
+	file.read(n_rows_bytes, 4);
+	file.read(n_cols_bytes, 4);
 
+	int n_images = (static_cast<unsigned char>(n_images_bytes[0]) << 24) | (static_cast<unsigned char>(n_images_bytes[1]) << 16) | (static_cast<unsigned char>(n_images_bytes[2]) << 8) | static_cast<unsigned char>(n_images_bytes[3]);
+	int n_rows = (static_cast<unsigned char>(n_rows_bytes[0]) << 24) | (static_cast<unsigned char>(n_rows_bytes[1]) << 16) | (static_cast<unsigned char>(n_rows_bytes[2]) << 8) | static_cast<unsigned char>(n_rows_bytes[3]);
+	int n_cols = (static_cast<unsigned char>(n_cols_bytes[0]) << 24) | (static_cast<unsigned char>(n_cols_bytes[1]) << 16) | (static_cast<unsigned char>(n_cols_bytes[2]) << 8) | static_cast<unsigned char>(n_cols_bytes[3]);
 
-    Layer(int n_inputs, int layer_size) {
-        for (int i = 0; i < layer_size; i++) {
-            node_arr.push_back(Node(n_inputs));
-        }
-    }
-};
+	std::cout << "n_images: " << n_images << "\nn_rows: " << n_rows << "\nn_cols: " << n_cols << "\n";
 
-class NeuralNet {
-public:
-    int n_inputs;
-    std::vector<Layer> layer_arr;
+	std::vector<std::vector<unsigned char>> images;
 
+	for (int i = 0; i < n_images; i++) {
+		std::vector<unsigned char> image(n_rows * n_cols);
 
-    NeuralNet(int in_n_inputs, std::vector<int>& layer_sizes) {
-        n_inputs = in_n_inputs;
+		file.read((char*)(image.data()), n_rows * n_cols);
+		
+		images.push_back(image);
+	}
 
-        int layer_inputs = n_inputs;
-        for (int i = 0; i < layer_sizes.size(); i++) {
-            layer_arr.push_back(Layer(layer_inputs, layer_sizes[i]));
-            layer_inputs = layer_sizes.at(i);
-        }
-    }
-};
+	file.close();
+	return images;
+}
+
+std::vector<std::vector<unsigned char>> readLabels(const std::string& filePath) {
+	std::ifstream file(filePath, std::ios::binary);
+
+	std::cout << "loading " << filePath << "\n";
+
+	char magic_number_bytes[4];
+	char n_labels_bytes[4];
+
+	file.read(magic_number_bytes, 4);
+	file.read(n_labels_bytes, 4);
+
+	int n_labels = (static_cast<unsigned char>(n_labels_bytes[0]) << 24) | (static_cast<unsigned char>(n_labels_bytes[1]) << 16) | (static_cast<unsigned char>(n_labels_bytes[2]) << 8) | static_cast<unsigned char>(n_labels_bytes[3]);
+
+	std::cout << "n_labels: " << n_labels << "\n";
+
+	std::vector<std::vector<unsigned char>> labels;
+
+	for (int i = 0; i < n_labels; i++) {
+		std::vector<unsigned char> label(1);
+
+		file.read((char*)(label.data()), 1);
+
+		labels.push_back(label);
+	}
+
+	file.close();
+	return labels;
+}
 
 int main()
 {
-    std::vector<int> network_sizes = { 16,16,10 };
-    NeuralNet network = NeuralNet(784, network_sizes);
+	std::vector<std::vector<unsigned char>> images = readImages(training_images_filepath);
+	std::vector<std::vector<unsigned char>> labels = readLabels(training_labels_filepath);
 
-    std::cout << network.layer_arr[0].node_arr[0].weights[0] << "\n";
-
-    return 1;
+	// display number in text
+	for (int i = 0; i < 28; i++) {
+		for (int j = 0; j < 28; j++) {
+			std::cout << (int)images[1][i * 28 + j] << " ";
+		}
+		std::cout << "\n";
+	}
+	// display number's label
+	std::cout << (int)labels[1][0] << "\n";
 }
+
 
