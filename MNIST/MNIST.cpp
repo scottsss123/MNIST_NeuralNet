b75e5ba62010ea7f2e192b4eadb397feeb2593aa
Scott Render <64731759+scottsss123@users.noreply.github.com>
2025-01-30 15:56:13 +0000

    new file:   .vscode/tasks.json 	new file:   MNIST/MNIST 	modified:   MNIST/MNIST.cpp 	new file:   MNIST/t10k-images.idx3-ubyte 	new file:   MNIST/t10k-labels.idx1-ubyte 	new file:   MNIST/train-images.idx3-ubyte 	new file:   MNIST/train-labels.idx1-ubyte

--- MNIST/MNIST.cpp
+++ MNIST/MNIST.cpp
@@ -1,19 +1,56 @@
+// --------------- includes ---------------------------------------------
 #include <iostream>
 #include <vector>
 #include <random>
 #include <fstream>
 
+// --------------- constants ---------------------------------------------
 const std::string training_images_filepath = "./train-images.idx3-ubyte";
 const std::string training_labels_filepath = "./train-labels.idx1-ubyte";
 
+const int n_pixels = 28 * 28;
+
+// --------------- global variables ---------------------------------------------
+std::vector<std::vector<unsigned char>> images;
+std::vector<std::vector<unsigned char>> labels;
 
 // random float gen, e.g. rand_real(e2);
 std::random_device rd;
 std::mt19937 e2(rd());
-std::uniform_real_distribution<> rand_real(0, 1); 
+std::uniform_real_distribution<> rand_real(-1, 1);
+std::uniform_real_distribution<> small_rand_real(-0.1, 0.1);
 
+// activation arrays 
+std::vector<float> a0(n_pixels);
+std::vector<float> a1(16);
+std::vector<float> a2(16);
+std::vector<float> a3(10);
 
-//28x28
+// weight arrays
+std::vector<float> w1(a0.size() * a1.size());
+std::vector<float> w2(a1.size() * a2.size());
+std::vector<float> w3(a2.size() * a3.size());
+
+// bias arrays
+std::vector<float> b1(16);
+std::vector<float> b2(16);
+std::vector<float> b3(10);
+
+// tweak arrays    tweaks to weights/biases in layer 0/1/2/3
+std::vector<float> tw1(a0.size() * a1.size());
+std::vector<float> tw2(a1.size() * a2.size());
+std::vector<float> tw3(a2.size() * a3.size());
+std::vector<float> tb1(a1.size());
+std::vector<float> tb2(a2.size());
+std::vector<float> tb3(a3.size());
+
+// activation 'tweaks'
+std::vector<float> at3(a3.size());
+std::vector<float> at2(a2.size());
+std::vector<float> at1(a1.size());
+std::vector<float> at0(a0.size());
+
+// --------------- functions ---------------------------------------------
 std::vector<std::vector<unsigned char>> readImages(const std::string& filePath) {
 	std::ifstream file(filePath, std::ios::binary);
 
@@ -78,20 +115,129 @@ std::vector<std::vector<unsigned char>> readLabels(const std::string& filePath) 
 	return labels;
 }
 
-int main()
-{
-	std::vector<std::vector<unsigned char>> images = readImages(training_images_filepath);
-	std::vector<std::vector<unsigned char>> labels = readLabels(training_labels_filepath);
-
-	// display number in text
-	for (int i = 0; i < 28; i++) {
-		for (int j = 0; j < 28; j++) {
-			std::cout << (int)images[1][i * 28 + j] << " ";
-		}
-		std::cout << "\n";
+void initialiseWeights() {
+	for (int i = 0; i < w1.size(); i++) {
+		w1[i] = rand_real(e2);
 	}
-	// display number's label
-	std::cout << (int)labels[1][0] << "\n";
+	for (int i = 0; i < w2.size(); i++) {
+		w2[i] = rand_real(e2);
+	}
+	for (int i = 0; i < w3.size(); i++) {
+		w3[i] = rand_real(e2);
+	}
+}
+
+void initialiseBiases() {
+	for (int i = 0; i < b1.size(); i++) {
+		b1[i] = small_rand_real(e2);
+	}
+	for (int i = 0; i < b2.size(); i++) {
+		b2[i] = small_rand_real(e2);
+	}
+	for (int i = 0; i < b3.size(); i++) {
+		b3[i] = small_rand_real(e2);
+	}
+}
+
+void populateInputs(int index) {
+	for (int i = 0; i < n_pixels; i++) {
+		float v = (float)images[index][i];
+		a0[i] = v / 255; // normalises greyscale between 0 and 1
+	}
+}
+
+int getLabel(int index) {
+	return (int)labels[index][0];
+}
+
+float sigmoid(float x) {
+	return 1 / (1 + std::exp(-x));
+}
+
+float inverse_sigmoid(float x) {
+	std::log(x / (1-x));
+}
+
+void outputVec(std::vector<float>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		std::cout << v[i] << ", ";
+	}
+	std::cout << "\n";
+}
+
+void forwardProp() {
+	// pixels -> layer 1
+	for (int i = 0; i < a1.size(); i++) {
+		float sum = 0;
+		for (int j = 0; j < a0.size(); j++) {
+			sum += a0[j] * w1[i * a0.size() + j];
+		}
+		sum += b1[i];
+		a1[i] = sigmoid(sum);
+	}
+	// layer 1 -> layer 2
+	for (int i = 0; i < a2.size(); i++) {
+		float sum = 0;
+		for (int j = 0; j < a1.size(); j++) {
+			sum += a1[j] * w2[i * a1.size() + j];
+		}
+		sum += b2[i];
+		a2[i] = sigmoid(sum);
+	}
+	// layer 2 -> layer 3
+	for (int i = 0; i < a3.size(); i++) {
+		float sum = 0;
+		for (int j = 0; j < a2.size(); j++) {
+			sum += a2[j] * w3[i * a2.size() + j];
+		}
+		sum += b3[i];
+		a3[i] = sigmoid(sum);
+	}
+}
+
+void backProp() {
+	// adds to tweak arrays how the cost most quickly decreases for current image
+	for (int j = 0; j < a3.size(); j++) {
+		for (int k = 0; k < a2.size(); k++) {
+			float wt = 0; // weight tweak
+
+			float delZdelW; // d(weighted sum) / d(weight)
+			float delAdelZ; // d(activation of jth node in last layer) / d(weighted sum)
+			float delCdelA; // d(cost) / d(activation of jth node in last layer)
+
+			delCdelA = 2 * (a3[j] - )
+
+			tw3[j * w3.size() + k] += wt;
+		}
+	}
+}
+
+float cost(int target) {
+	float sum = 0;
+	std::vector<float> target_arr(a3.size());
+	target_arr[target] = 1;
+
+	// mean squared error
+	for (int i = 0; i < a3.size(); i++) {
+		sum += std::pow((target_arr[i] - a3[i]), 2);
+	}
+
+	return sum;
 }
 
 
+
+// --------------- main ---------------------------------------------
+int main()
+{
+	images = readImages(training_images_filepath);
+	labels = readLabels(training_labels_filepath);
+
+	initialiseWeights();
+	initialiseBiases();
+
+	populateInputs(1);
+	forwardProp();
+	outputVec(a3);
+	std::cout << "cost: " << cost(getLabel(1));
+}
